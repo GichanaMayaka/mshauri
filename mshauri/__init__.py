f92feb87d6f3c14f16323cc382a0f64e381085b8
Gichana <gichanamayaka@gmail.com>
2024-08-10 15:27:24 +0300

    added flassger for api documentation.

--- mshauri/__init__.py
+++ mshauri/__init__.py
@@ -1,18 +1,14 @@
-import atexit
 from http import HTTPStatus
 
-import pandas as pd
-from apscheduler.jobstores.base import ConflictingIdError
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
+from flasgger import Swagger
 from flask import Flask, request
 from pydantic import PostgresDsn
 
 from config import configs
 from mshauri.commands import create_db, drop_db, drop_tables
+from mshauri.controllers import bp
 from mshauri.extensions import db, migrate
-from mshauri.models import MentorsChecklist
-from mshauri.transformer import parser
+from mshauri.models import CME, Drill, MentorsChecklist
 
 SOURCE = "./mshauri/dataset/data.xlsx"  # Path to the dataset
 
@@ -28,23 +24,13 @@ def create_app(database_url: PostgresDsn = configs.POSTGRES_DSN) -> Flask:
         Flask: The resultant Flask instance
     """
     app = Flask(__name__)
+    swag = Swagger(app, template_file="./controllers/api-docs.yml")
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url.unicode_string()
     app.config["SECRET_KEY"] = configs.SECRET_KEY
 
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(
-        func=task,
-        args=[SOURCE, app],
-        trigger="interval",
-        seconds=300,
-        id="parse_dataset",
-    )
-    scheduler.start()
-
-    atexit.register(lambda: scheduler.shutdown(wait=True))
-
     register_extensions(app)
     register_commands(app)
+    register_blueprints(app)
 
     @app.route("/", methods=["GET"])
     def index() -> tuple[str, int]:
@@ -53,57 +39,14 @@ def create_app(database_url: PostgresDsn = configs.POSTGRES_DSN) -> Flask:
             HTTPStatus.OK,
         )
 
-    @app.route("/checklists", methods=["GET"])
-    def get_checklists() -> tuple[dict, int]:
-        checklists = MentorsChecklist.query.all()
-
-        if checklists:
-            return {"checklists": checklists}, HTTPStatus.OK
-
-        return {"message": "Not Found"}, HTTPStatus.NOT_FOUND
-
-    @app.route("/schedule", methods=["POST"])
-    def modify_trigger() -> tuple[dict, int]:
-        data = request.get_json()
-        trigger = data.get("schedule")
-
-        if not trigger:
-            return {
-                "message": "Please specify a Cron expression under the 'schedule' key"
-            }, HTTPStatus.BAD_REQUEST
-
-        try:
-            cron_trigger = CronTrigger.from_crontab(trigger)
-            job = scheduler.get_job("parse_dataset")
-
-            if job:
-                job.modify(trigger=cron_trigger)
-                message = "Job trigger modified successfully!"
-
-            else:
-                # If the job doesn't exist, add a new job
-                message = "No jobs currently scheduled"
-                return {
-                    "error": "No Job[s] found",
-                    "detail": message,
-                }, HTTPStatus.NOT_FOUND
-
-        except ValueError as e:
-            return {
-                "error": "Invalid cron expression",
-                "details": str(e),
-            }, HTTPStatus.BAD_REQUEST
-
-        except ConflictingIdError:
-            return {"error": "Job ID conflict"}, HTTPStatus.BAD_REQUEST
-
-        return {"message": message}, HTTPStatus.OK
-
     @app.after_request
     def set_headers(response):
         response.headers["Access-Control-Allow-Origin"] = configs.ALLOWED_ORIGINS
         response.headers["Access-Control-Allow-Methods"] = "GET, POST"
-        response.headers["Content-Type"] = "application/json"
+
+        if "flasgger.apidocs" not in request.endpoint:
+            response.headers["Content-Type"] = "application/json"
+
         response.headers["Access-Control-Allow-Headers"] = "Origin, Content-Type"
         return response
 
@@ -134,8 +77,5 @@ def register_commands(app: Flask) -> None:
         app.cli.command()(command)
 
 
-def task(source: str, app: Flask) -> None:
-    """Task that performs the transformation"""
-    with app.app_context():
-        df = pd.read_excel(source)
-        parser(df)
+def register_blueprints(app: Flask) -> None:
+    app.register_blueprint(bp)
--- mshauri/extensions.py
+++ mshauri/extensions.py
@@ -1,5 +1,7 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
 
 db = SQLAlchemy()
 migrate = Migrate()
+swagger = Swagger()
